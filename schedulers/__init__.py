9dd63587caffa6a330ac9d4d002fdf4dcee357ba
Nikita Kostin <nikita.kostin@tum.de>
2022-06-15 01:56:46 +0200

    Renaming unit jobs' scheduler file according to the classname

--- schedulers/__init__.py
+++ schedulers/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from .abstract_scheduler import AbstractScheduler
 from .flow_scheduler import FlowScheduler
-from .unit_length_scheduler import UnitJobsScheduler
+from .unit_jobs_scheduler import UnitJobsScheduler
 
 __all__ = [
     AbstractScheduler,
