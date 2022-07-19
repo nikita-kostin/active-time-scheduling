44d178fb5c9e69e0e8201bdc3bee8ffdbe336f2c
Nikita Kostin <nikita.kostin@tum.de>
2022-07-20 01:45:46 +0200

    Fix UDCS tests

--- tests/schedulers/test_matching_scheduler.py
+++ tests/schedulers/test_matching_scheduler.py
@@ -46,16 +46,14 @@ class TestMatchingScheduler(object):
 
     @pytest.mark.repeat(1000)
     def test_udcs_against_brute_force(self) -> None:
-        max_duration = randint(1, 4)
         max_t = randint(4, 8)
-        max_concurrency = randint(1, 3)
-        jobs_count = randint(max(1, max_t // max_duration // 2), max_t // max_duration * max_concurrency)
+        jobs_count = randint(max_t // 2, max_t * 2)
 
-        jobs = list(islice(JobsGenerator(max_duration, max_t), jobs_count))
+        jobs = list(islice(JobsGenerator(1, max_t), jobs_count))
         job_pool = JobPoolMI()
         job_pool.jobs = jobs
 
         schedule_a = BruteForceScheduler().process(job_pool, 2)
         schedule_b = UpperDegreeConstrainedSubgraphScheduler().process(job_pool)
 
-        check_equality(schedule_a, schedule_b, job_pool, max_concurrency)
+        check_equality(schedule_a, schedule_b, job_pool, 2)
