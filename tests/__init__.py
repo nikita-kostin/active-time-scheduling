efd15975a30d8673001e5eeafb0fca9b6c0850e0
Nikita Kostin <nikita.kostin@tum.de>
2022-08-23 19:14:30 +0200

    Fix __init__ for test folder

---
+++ tests/__init__.py
@@ -0,0 +1,18 @@
+from .schedulers import (
+    generate_feasible_jobs_normal_distribution,
+    generate_feasible_jobs_uniform_distribution,
+    generate_feasible_mi_jobs,
+    generate_jobs_normal_distribution,
+    generate_jobs_uniform_distribution,
+    generate_mi_jobs,
+)
+
+
+__all__ = [
+    generate_feasible_jobs_normal_distribution,
+    generate_feasible_jobs_uniform_distribution,
+    generate_feasible_mi_jobs,
+    generate_jobs_normal_distribution,
+    generate_jobs_uniform_distribution,
+    generate_mi_jobs,
+]
---
+++ tests/schedulers/__init__.py
@@ -0,0 +1,22 @@
+from .common import (
+    check_2_approximation,
+    check_equality,
+    generate_feasible_jobs_normal_distribution,
+    generate_feasible_jobs_uniform_distribution,
+    generate_feasible_mi_jobs,
+    generate_jobs_normal_distribution,
+    generate_jobs_uniform_distribution,
+    generate_mi_jobs,
+)
+
+
+__all__ = [
+    check_2_approximation,
+    check_equality,
+    generate_feasible_jobs_normal_distribution,
+    generate_feasible_jobs_uniform_distribution,
+    generate_feasible_mi_jobs,
+    generate_jobs_normal_distribution,
+    generate_jobs_uniform_distribution,
+    generate_mi_jobs,
+]
